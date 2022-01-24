bdc9429026d7ad0e97d6f402751d9a872568a5cd
midCenturyDude7 <mgriffes@gmail.com>
2022-01-23 20:18:55 -0800

    Building the endpoints

--- app.py
+++ app.py
@@ -23,10 +23,65 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Credentials', 'true')
     return response
 
-@app.route('/ping')
-def ping():
-    return ("Hello, World!")
+
+class ContactModel(db.model):
+    __tablename__ = "table"
 
 
-if __name__ == '__main__':
-    app.run()
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+
+
+    def __init__(self, name):
+        self.name = name
+    
+
+    def __repr__(self):
+        return f"{self.name}"
+
+
+@app.before_first_request
+def create_table():
+    db.create_all()
+
+
+@app.route('/data/create', methods = ['GET', 'POST'])
+def create():
+    if request.method == 'GET':
+        return jsonify({"success": True, "message": "this is the create endpoint"}), 201
+    
+    if request.method == 'POST':
+        request.data = json.loads(request.data)
+        name = request_data['name']
+        contact = ContactModel(name=name)
+        db.session.add(contact)
+        db.session.commit()
+        return jsonify({"sucess": True, "message": "contact added successfully"}), 201
+
+
+def contact_serializer(contact):
+    return {'name': contact.name}
+
+
+@app.route('/data/delete', methods=['GET', 'POST'])
+def delete():
+    request_data = json.loads(request.data)
+    name = request_data['name']
+    contact = ContactModel.query.filter_by(name=name).first()
+    if request.method == 'POST':
+        if contact:
+            db.session.delete(contact)
+            db.session.commit()
+            return jsonify({"success": True, "message": "contact deleted successfully"}), 201
+        abort(404)
+
+    return jsonify({"success": True}), 201
+
+
+# @app.route('/ping')
+# def ping():
+#     return ("Hello, World!")
+
+
+# if __name__ == '__main__':
+#     app.run()
